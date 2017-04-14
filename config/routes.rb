f79967e55035db412b375d219df0aff3bfe47512
James <bayougiant@gmail.com>
2017-04-14 15:45:38 -0500

    Create rails app of Steepsteel

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pages#home"
+  get "about",to:"pages#about"
+  get "auctions",to:"pages#auctions"
+  get "listings",to:"pages#listings"
+  get "sitemanagement",to:"pages#sitemanagement"
+ 
+end
